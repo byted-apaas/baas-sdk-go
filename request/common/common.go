ed12463fee3ad3831f4c4eb2ed89d44f2da6fe35
zhouwexin <zhouwexin@bytedance.com>
2023-08-07 10:50:21 +0800

    feat: async function add unauth_field

--- request/common/common.go
+++ request/common/common.go
@@ -4,25 +4,26 @@ import (
 	"context"
 
 	cExceptions "github.com/byted-apaas/server-common-go/exceptions"
+	cHttp "github.com/byted-apaas/server-common-go/http"
 	cUtils "github.com/byted-apaas/server-common-go/utils"
 )
 
-func BuildInvokeParamsStr(ctx context.Context, apiName string, params interface{}) (map[string]interface{}, error) {
-	sysParams, bizParams, err := BuildInvokeParamAndContext(ctx, params)
+func BuildInvokeParamsStr(ctx context.Context, funcAPIName string, params interface{}, needPermission bool) (map[string]interface{}, error) {
+	sysParams, bizParams, err := BuildInvokeParamAndContext(ctx, params, funcAPIName, needPermission)
 	if err != nil {
 		return nil, err
 	}
 
 	return map[string]interface{}{
-		"apiAlias":    apiName,
+		"apiAlias":    funcAPIName,
 		"params":      bizParams,
 		"context":     sysParams,
 		"triggerType": "workflow",
 	}, nil
 }
 
-func BuildInvokeParamAndContext(ctx context.Context, params interface{}) (string, string, error) {
-	sysParams, _ := cUtils.JsonMarshalBytes(BuildInvokeSysParams(ctx))
+func BuildInvokeParamAndContext(ctx context.Context, params interface{}, funcAPIName string, needPermission bool) (string, string, error) {
+	sysParams, _ := cUtils.JsonMarshalBytes(BuildInvokeSysParams(ctx, params, funcAPIName, needPermission))
 
 	bizParams, err := cUtils.JsonMarshalBytes(params)
 	if err != nil {
@@ -32,10 +33,15 @@ func BuildInvokeParamAndContext(ctx context.Context, params interface{}) (string
 	return string(sysParams), string(bizParams), nil
 }
 
-func BuildInvokeSysParams(ctx context.Context) map[string]interface{} {
-	return map[string]interface{}{
+func BuildInvokeSysParams(ctx context.Context, params interface{}, funcAPIName string, needPermission bool) map[string]interface{} {
+	v := map[string]interface{}{
 		"triggertaskid":             cUtils.GetTriggerTaskIDFromCtx(ctx),
 		"x-kunlun-distributed-mask": cUtils.GetDistributedMaskFromCtx(ctx),
 		"x-kunlun-loop-masks":       cUtils.GetLoopMaskFromCtx(ctx),
 	}
+
+	if needPermission {
+		v["permission"] = cHttp.CalcParamsNeedPermission(ctx, funcAPIName, "input", params)
+	}
+	return v
 }
